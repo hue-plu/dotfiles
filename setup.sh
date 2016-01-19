bfb84be93687d7c2bbe42437ccd97af4dc2b31c9
hue-plu <shuetwitter@gmail.com>
2016-01-19 11:25:32 +0900

    for osx: add karabiner

--- setup.sh
+++ setup.sh
@@ -33,4 +33,18 @@ brew tap caskroom/versions
 brew update
 
 brew cask install amethyst
+cat ./.amethyst > "~/.amethyst"
 
+mkdir -p "$HOME/Library/Application Support/Karabiner"
+cat ./private.xml > "$HOME/Library/Application Support/Karabiner/private.xml"
+
+KARABINER="/Applications/Karabiner.app/Contents/Library/bin/karabiner"
+if [[ -e "$KARABINER" ]]; then
+	$KARABINER reloadxml
+	$KARABINER set parameter.keyoverlaidmodifier_timeout 300
+	$KARABINER set private.vim_keybind_apps_esc_with_eisuu
+	$KARABINER set repeat.initial_wait 300
+	$KARABINER set repeat.wait 40
+else
+	echo "==> Warning: Karabiner.app not found < https://pqrs.org/osx/karabiner/index.html.en >"
+fi
