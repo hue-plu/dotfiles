9f1f46e382f9fadbb03d2262fef1c97b7e3141ad
hue-plu <shuetwitter@gmail.com>
2016-03-09 13:02:14 +0900

    improve slate

--- .slate.js
+++ .slate.js
@@ -1,12 +1,19 @@
-S.bind('1:ctrl', function (win) {
-    win.doOperation(S.operation('focus', { app: 'Firefox' }));
-});
-S.bind('2:ctrl', function (win) {
-    win.doOperation(S.operation('focus', { app: 'Slack' }));
-});
-S.bind('3:ctrl', function (win) {
-    win.doOperation(S.operation('focus', { app: 'Wunderlist' }));
-});
-S.bind('4:ctrl', function (win) {
-    win.doOperation(S.operation('focus', { app: 'iTerm' }));
-});
+var launch_and_focus = function (target) {
+    return function (win) {
+        var apps = [];
+        S.eachApp(function (app) { apps.push(app.name()); });
+        if (! _.find(apps, function (name) { return name === target; })) {
+            win.doOperation(
+                S.operation('shell', {
+                    command: "/usr/bin/open -a " + target,
+                    waithFoeExit: true
+                })
+            );
+        }
+        win.doOperation(S.operation('focus', { app: target }));
+    };
+};
+S.bind('1:ctrl', launch_and_focus('Firefox'));
+S.bind('2:ctrl', launch_and_focus('Slack'));
+S.bind('3:ctrl', launch_and_focus('Wunderlist'));
+S.bind('4:ctrl', launch_and_focus('iTerm'));
