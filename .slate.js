c2a69c08d74045fc0a64cd323bfc32286d4cc55b
hue-plu <shuetwitter@gmail.com>
2018-10-03 17:28:31 +0900

    add neotree theme

--- .slate.js
+++ .slate.js
@@ -17,5 +17,5 @@ S.bind('u:cmd', launch_and_focus('Vivaldi'));
 S.bind('i:cmd', launch_and_focus('Slack'));
 // S.bind('o:cmd', launch_and_focus('FirefoxDeveloperEdition'));
 S.bind('j:cmd', launch_and_focus('Alacritty'));
-// S.bind('h:cmd', launch_and_focus('Emacs'));
+S.bind('h:cmd', launch_and_focus('Emacs'));
 // S.bind('g:cmd', launch_and_focus('Sublime Text'));
