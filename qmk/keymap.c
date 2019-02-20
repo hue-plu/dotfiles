ef8d1eaacdebf0cc9a502adc78d6a6d08cf2621c
hue-plu <shuetwitter@gmail.com>
2019-02-20 11:55:45 +0900

    change qmk keybind

--- qmk/keymap.c
+++ qmk/keymap.c
@@ -65,19 +65,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Tab  |   Q  |   W  |   E  |   R  |   T  |             |   Y  |   U  |   I  |   O  |   P  | \    |
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
-   * | Ctrl |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  | Enter|
+   * | Caps |   A  |   S  |   D  |   F  |   G  |             |   H  |   J  |   K  |   L  |   ;  | Enter|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |   [  |   ]  |   N  |   M  |   ,  |   .  |   /  | Shift|
    * |------+------+------+------+------+------+------+------+------+------+------+------+------+------|
-   * |Adjust| LOWER| Alt  | GUI  | EISU |Space |Space |Space |Space | LOWER|      |      |  Alt |      |
+   * | Ctrl | LOWER| Alt  | GUI  | EISU |Space |Adjust|Space |Space | LOWER|      |      |  Alt |      |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = KEYMAP( \
       KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,   \
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
-      KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,    \
+      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,    \
       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LBRC, KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT ,  \
-      ADJUST,  LOWER,   KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  KC_SPC,  LOWER,   _______, _______, KC_RALT, _______    \
+      KC_LCTL, LOWER,   KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  ADJUST,  KC_SPC,  KC_SPC,  LOWER,   _______, _______, KC_RALT, _______    \
       ),
   /* Lower
    * ,-----------------------------------------.             ,-----------------------------------------.
